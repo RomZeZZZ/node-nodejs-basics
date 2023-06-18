fbcbb77878debf07a9337572afd0782ea397b5cf
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-18 14:51:49 +0300

    feat: fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { appendFile, access, constants } from 'fs/promises';
 const create = async () => {
-    // Write your code here 
+  try {
+    const filePath = './src/fs/files/fresh.txt';
+    const fileExists = await access(filePath, constants.F_OK)
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      appendFile(filePath, 'I am fresh and young')
+      .then(() => {
+        console.log('Fresh file created successfully.');
+      })
+      .catch(() => {
+        console.log('FS operation failed. Failed to create file');
+      });
+      return false;
+    });
+    if (fileExists) {
+      throw new Error('FS operation failed. File already exists.');
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await create();
