b8d61ce732f0bb0071cbc912d345ca2b465bba77
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-19 01:16:39 +0300

    feat: add src/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const rootPath = dirname(fileURLToPath(import.meta.url));
+        const filePath = join(rootPath, 'files', 'fileToCalculateHashFor.txt');
+        const fileData = await readFile(filePath)
+            .then((data) => data)
+            .catch(() => {
+                throw new Error(`FS operation failed. Failed to read ${filePath}`);
+            });
+        const hash = createHash('sha256');
+        hash.update(fileData);
+        const hashHex = hash.digest('hex');
+        console.log(`SHA256 Hash: ${hashHex}`);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 await calculateHash();
