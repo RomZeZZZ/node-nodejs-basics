bba22ab4d7a071b9593877035abd4e9a92677b96
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-18 18:32:56 +0300

    feat: add fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { unlink } from 'fs/promises';
 const remove = async () => {
-    // Write your code here 
+    const filePath = './src/fs/files/fileToRemove.txt';
+    try {
+        await unlink(filePath)
+        .then(() => console.log('file delete successfully...'))
+        .catch(() => {
+            throw new Error(`FS operation failed. Failed to delete ${filePath}`);
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 await remove();
