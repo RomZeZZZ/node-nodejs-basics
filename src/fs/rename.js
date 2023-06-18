d7c5c2cf389770c53939bb8592e3a4a0c9015a47
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-18 18:09:08 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
-const rename = async () => {
-    // Write your code here 
+import { access, constants, rename } from 'fs/promises';
+const reName = async () => {
+    const filePath = './src/fs/files/wrongFilename.txt';
+    const fileNewPath = './src/fs/files/properFilename.md';
+    try {
+        const wrongFileExist = await access(filePath, constants.F_OK)
+        .then(() => { return true })
+        .catch(() => { return false });
+        const properFileExist = await access(fileNewPath, constants.F_OK)
+        .then(() => { return true })
+        .catch(() => { return false });
+        if (wrongFileExist && !properFileExist) {
+            await rename(filePath, fileNewPath);
+        } else if (properFileExist){
+            throw new Error('FS operation failed. properFilename.md is already exist.');
+        } else {
+            throw new Error('FS operation failed. wrongFilename.txt doesnt exist.');
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+   
 };
 
-await rename();
+await reName();
