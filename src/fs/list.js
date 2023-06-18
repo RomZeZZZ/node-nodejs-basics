f83a16d48711622bd74a2ad77b5ffb32f1507728
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-18 18:42:00 +0300

    feat: add fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir, mkdir, writeFile, access, constants, readFile } from 'fs/promises';
 const list = async () => {
-    // Write your code here 
+    const folderPath = './src/fs/files/';
+    try {
+        const folderExist = await access(folderPath, constants.F_OK)
+        .then(() => { return true })
+        .catch(() => { return false });
+        const files = await readdir(folderPath)
+        .then((data) => data)
+        .catch(() => { throw new Error('FS operation failed. Required directory is missing') });
+        if (folderExist) {
+            files.length === 0 ? console.log('Folder is empty') : files.forEach(item => console.log(item));
+        } else {
+            throw new Error('FS operation failed. Folder doesnt exist.');
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
 };
 
 await list();
