e1a25abc9614a10f851a6701e8f2210bfaa22337
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-19 14:35:33 +0300

    feat: add streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 const write = async () => {
-    // Write your code here 
+    const rootPath = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(rootPath, 'files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
 };
 
 await write();
